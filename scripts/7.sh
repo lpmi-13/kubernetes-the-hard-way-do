04f344a394b4846c210346d54d4d5f04c68004cd
Adam Leskis <leskis@gmail.com>
2021-06-13 21:11:47 +0100

    add script for bootstrapping etcd

---
+++ scripts/7.sh
@@ -0,0 +1,9 @@
+# this runs a local script on each of the remote controller hosts
+for instance in controller-0 controller-1 controller-2; do
+  external_ip=$(doctl compute droplet list ${instance} \
+    --output json | jq -cr '.[].networks.v4 | .[] | select(.type == "public") | .ip_address')
+
+  ssh -i kubernetes.id_rsa \
+  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+  root@$external_ip < ./scripts/bootstrap_etcd_on_controllers.sh
+done
