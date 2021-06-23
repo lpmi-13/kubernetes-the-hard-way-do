ef7119c1892e05419c27b7e80280d84768d2150e
Adam Leskis <leskis@gmail.com>
2021-06-23 05:15:45 +0100

    add some sleeps so the terminal output is easier to watch

--- scripts/12.sh
+++ scripts/12.sh
@@ -1,10 +1,18 @@
 kubectl create -f https://raw.githubusercontent.com/lpmi-13/kubernetes-the-hard-way-do/main/deployments/core-dns.yaml
 
+sleep 5
+
 kubectl get pods -l k8s-app=kube-dns -n kube-system
+
+sleep 5
 
 kubectl run busybox --image=busybox:1.28.4 --restart=Never -- sleep 3600
 
+sleep 5
+
 kubectl get pod busybox
+
+sleep 5
 
 kubectl exec -it busybox -- nslookup kubernetes
 
